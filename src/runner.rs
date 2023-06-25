89931bc93536a655ba10b8e63920f1fc7ade8304
Matt Ellis <mathew.ellis2@griffithuni.edu.au>
2023-06-25 12:49:39 +1000

    tidied up base runner class

--- src/runner.rs
+++ src/runner.rs
@@ -1,32 +1,15 @@
-use std::error::Error;
-use std::fs::{File, read_to_string, self};
+use std::fs;
 use std::path::Path;
-use std::rc::Rc;
-use std::cell::RefCell;
 
-pub struct Runner {
-    // stdout: Rc<RefCell<Writer>>,
-    // stderr: Rc<RefCell<Writer>>,
-}
+pub struct Runner {}
 
 impl Default for Runner {
     fn default() -> Self {
-        // Runner::new(
-        //     Rc::new(RefCell::new(Writer::StdOut(io::BufWriter::new(io::stdout())))),
-        //     Rc::new(RefCell::new(Writer::StdErr(io::BufWriter::new(io::stderr())))),
-        // )
         Runner{}
     }
 }
 
 impl Runner {
-    // pub fn new(stdout: Rc<RefCell<Writer>>, stderr:Rc<RefCell<Writer>>) -> Self {
-    //     Runner {
-    //         stdout, 
-    //         stderr
-    //     }       
-    // }
-
     pub fn file(&mut self, f: &Path) -> Result<(), String> {
         
         let src = fs::read_to_string(f).unwrap_or_default();
